07acb4a175150fd5d8d6154a671528052ecc4d73
Ritesh Tumbade <96604461+RiteshTumbade@users.noreply.github.com>
2023-05-17 10:03:31 +0530

    init

---
+++ src/app/impediments/impediments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-impediments',
+  templateUrl: './impediments.component.html',
+  styleUrls: ['./impediments.component.css']
+})
+export class ImpedimentsComponent {
+
+}
