f2b729d03fab68faa70e97ad796b35634914efe3
Ritesh Tumbade <96604461+RiteshTumbade@users.noreply.github.com>
2023-05-21 14:24:23 +0530

    data

--- src/app/classes/release-sprint-backlog.ts
+++ src/app/classes/release-sprint-backlog.ts
@@ -1,5 +1,186 @@
 export class ReleaseSprintBAcklog {
-    releaseSprintBacklogId : number
+    releaseSprintBacklogId: number
+    releaseSprintId: {
+        releaseSprintId:number,
+        sprintName: string,
+        releaseId: {
+            releaseId: number,
+            releaseName: string,
+            projectId: number
+        },
+        startDate: any,
+        endDate: any,
+        totalEstimatedStoryPoint: number
+    }
+    backlogItemParentChildId: {
+        backolgItemParentChildId: number,
+        backlogItemParentId: {
+            backlogItemId: number,
+            backlogName: string,
+            backlogTypeId: {
+                backlogTypeId: number,
+                backlogTypeName: string
+            },
+            projectId: {
+                projectId: number,
+                projectName: string,
+                companyName: string,
+                createdOn: any,
+                createdBy: {
+                    userId: number,
+                    userFirstName: string,
+                    userlastName: string,
+                    emailId: string,
+                    password: string,
+                    contactNumber: string,
+                companyName: null,
+                    countrycode: string
+                }
+            },
+            who: {
+                projectUserRoleId: number,
+                projectId: {
+                    projectId: number,
+                    projectName: string,
+                    companyName: string
+                    createdOn: any,
+                    createdBy: {
+                        userId: number,
+                        userFirstName: string,
+                        userlastName: string,
+                        emailId: string,
+                        password: string,
+                        contactNumber: string,
+                        companyName: null,
+                        countrycode: string
+                    }
+                },
+                userId: {
+                    userId: number,
+                    userFirstName: string,
+                    userlastName: string,
+                    emailId: string,
+                    password: string,
+                    contactNumber: string,
+                    companyName: null,
+                    countrycode: string
+                },
+                roleId: {
+                    roleId: number,
+                    roleName: string
+                }
+            },
+            what: null,
+            benefits: null,
+            acceptenceCriteria: null,
+            description: null
+        },
+        backlogItemChildId: {
+            backlogItemId: number,
+            backlogName: string,
+            backlogTypeId: {
+                backlogTypeId: number,
+                backlogTypeName: string
+            },
+            projectId: {
+                projectId: number,
+                projectName: string,
+                companyName: string,
+                createdOn:any,
+                createdBy: {
+                    userId: number,
+                    userFirstName: string,
+                    userlastName: string,
+                    emailId: string,
+                    password:string,
+                    contactNumber: string,
+                    companyName: null,
+                    countrycode: string
+                }
+            },
+            who: {
+                projectUserRoleId: number,
+                projectId: {
+                    projectId: number,
+                    projectName: string,
+                    companyName:string,
+                    createdOn: any,
+                    createdBy: {
+                        userId: number,
+                        userFirstName: string,
+                        userlastName: string,
+                        emailId: string,
+                        password: string,
+                        contactNumber: string,
+                        companyName: null,
+                        countrycode: string
+                    }
+                },
+                userId: {
+                    userId: number,
+                    userFirstName: string,
+                    userlastName: string,
+                    emailId: string,
+                    password:string,
+                    contactNumber: string,
+                    companyName: null,
+                    countrycode: string
+                },
+                roleId: {
+                    roleId: number,
+                    roleName: string
+                }
+            },
+            what: null,
+            benefits: null,
+            acceptenceCriteria: null,
+            description: null
+        },
+        status: {
+            statusId: number,
+            statusName: string,
+            sequence: number
+        }
+    }
+    estimateStoryPoint: number
+    assignedTo: {
+        projectUserRoleId: number,
+        projectId: {
+            projectId: number,
+            projectName: string,
+            companyName: string,
+            createdOn: any,
+            createdBy: {
+                userId: number,
+                userFirstName: string,
+                userlastName: string,
+                emailId: string,
+                password: string,
+                contactNumber: string,
+                companyName: null,
+                countrycode: string
+            }
+        }
+        userId: {
+            userId: number,
+            userFirstName:string,
+            userlastName: string,
+            emailId: string,
+            password: string,
+            contactNumber: string,
+            companyName: null,
+            countrycode: string
+        },
+        roleId: {
+            roleId: number,
+            roleName: string
+        }
+    }
+    status: {
+        statusId: number,
+        statusName: string,
+        sequence: number
+    }
 }
     
 
